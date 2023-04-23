e49a965c31065087a24ec44762c1c35dc33f483e
jyf-111 <1920950476@qq.com>
2023-04-23 23:46:10 +0800

    refactor(context): unexpect at rigit place

--- src/context.rs
+++ src/context.rs
@@ -26,8 +26,8 @@ impl Context {
             data.insert("room_id", room_id.as_str());
 
             info!("querying room {}", room_name);
-            let resp =
-                client::handle_post(def::DEVICE_URL.as_str(), def::HEADERMAP.clone(), data).ok()?;
+            let resp = client::handle_post(def::DEVICE_URL.as_str(), def::HEADERMAP.clone(), data)
+                .expect("net error when querying room");
 
             match client::get_name_info(resp, name.clone()) {
                 Some(info) => return Some(info),
@@ -46,35 +46,37 @@ impl Context {
         let date = time::get_date("%Y-%m-%d");
         body.insert("date", date.as_str());
 
-        let resp =
-            client::handle_post(def::DEVICE_URL.as_str(), def::HEADERMAP.clone(), body).ok()?;
-        return client::get_site_info(resp);
+        let resp = client::handle_post(def::DEVICE_URL.as_str(), def::HEADERMAP.clone(), body)
+            .expect("net error when querying site");
+
+        client::get_site_info(resp)
     }
 
-    pub fn login(&self, username: String, password: String) -> Option<(String, String, String)> {
-        let student = Student::new(username.clone(), password.clone());
-        student.save_to_file().ok()?;
-
+    fn handle_login(&self, username: String, password: String) -> Option<(String, String, String)> {
         let mut body = HashMap::new();
         body.insert("act", "login");
         body.insert("id", username.as_str());
         body.insert("pwd", password.as_str());
-        let resp =
-            client::handle_post(def::LOGIN_URL.as_str(), def::HEADERMAP.clone(), body).ok()?;
+        let resp = client::handle_post(def::LOGIN_URL.as_str(), def::HEADERMAP.clone(), body)
+            .expect("net error when login");
 
-        return client::get_login_info(resp);
+        client::get_login_info(resp)
     }
 
-    pub fn status(& self) -> Option<(String, String, String,String)> {
-        let mut student = Student::new("".to_string(), "".to_string());
-        student.read_from_file().ok()?;
+    pub fn login(&self, username: String, password: String) -> Option<(String, String, String)> {
+        let student = Student::new(username.clone(), password.clone());
+        student.save_to_file().expect("save student info failed");
+        self.handle_login(username, password)
+    }
 
+    pub fn status(&self) -> Option<(String, String, String, String)> {
         //login
-        let mut body = HashMap::new();
-        body.insert("act", "login");
-        body.insert("id", student.username());
-        body.insert("pwd", student.password());
-        client::handle_post(def::LOGIN_URL.as_str(), def::HEADERMAP.clone(), body).ok()?;
+        let mut student = Student::new("".to_string(), "".to_string());
+        student.read_from_file().expect("read student info failed");
+        self.handle_login(
+            student.username().to_string(),
+            student.password().to_string(),
+        );
 
         let mut body = HashMap::new();
         body.insert("act", "get_History_resv");
@@ -84,6 +86,6 @@ impl Context {
         let resp =
             client::handle_post(def::CENTER_URL.as_str(), def::HEADERMAP.clone(), body).ok()?;
 
-        return client::get_status_info(resp);
+        client::get_status_info(resp)
     }
 }
