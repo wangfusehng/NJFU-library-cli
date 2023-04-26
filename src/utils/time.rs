152a395ad01f1c5d3f8d9db81934ed43c5454925
jyf-111 <1920950476@qq.com>
2023-04-26 22:29:30 +0800

    fix(time): fix time zoom to +8

--- src/utils/time.rs
+++ src/utils/time.rs
@@ -1,14 +1,22 @@
 use chrono::prelude::*;
-use chrono::{DateTime, Local};
+use chrono::{DateTime, Utc};
 
 /// get format date
 pub fn get_date_today(fmt: &str) -> String {
-    let utc: DateTime<_> = Local::now();
+    // Time zone offset
+    let offset: chrono::FixedOffset =
+        FixedOffset::east_opt(8 * 3600).expect("time zone offset fail");
+
+    let utc: DateTime<_> = Utc::now().with_timezone(&offset);
     utc.format(fmt).to_string()
 }
 
 pub fn get_date_tomorrow(fmt: &str) -> String {
-    let dt = Local::now().timestamp_millis();
+    // Time zone offset
+    let offset: chrono::FixedOffset =
+        FixedOffset::east_opt(8 * 3600).expect("time zone offset fail");
+
+    let dt = Utc::now().with_timezone(&offset).timestamp_millis();
     let n_day = 1000 * 60 * 60 * 24;
     let now = dt + n_day;
     let local = Local.timestamp_millis_opt(now);
